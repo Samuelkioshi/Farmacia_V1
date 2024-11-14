ac6edbb61dfa7d33e6ad94142f898a85f0282e0d
Your Name <you@example.com>
2024-11-13 21:10:09 -0300

    update: comentarios

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pyodbc
 app = Flask(__name__)
 app.secret_key = 'chave_secreta'
 
-# Configuração da conexão com o banco de dados
 conn_str = (
     "DRIVER={SQL Server};"
     "SERVER=Samuel;"
@@ -38,24 +37,20 @@ def home():
     conn.close()
     return render_template('home.html', produtos=produtos, clientes=clientes, search_query=search_query)
 
-# Rota da página de administração
 @app.route('/admin', methods=['GET'])
 def admin_access():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # Consulta todos os produtos
     cursor.execute("SELECT ID_PROD, NOME_PROD, SECAO, VALOR, DESCRICAO FROM TB_PRODUTOS")
     produtos = cursor.fetchall()
 
-    # Consulta todos os clientes
     cursor.execute("SELECT ID, NOME, CPF, TELEFONE, EMAIL FROM TB_CLIENTES")
     clientes = cursor.fetchall()
 
     conn.close()
     return render_template('admin.html', produtos=produtos, clientes=clientes)
 
-# Rotas para manipulação de produtos
 @app.route('/adicionar_produto', methods=['POST'])
 def adicionar_produto():
     nome = request.form['nome']
@@ -88,7 +83,6 @@ def excluir_produto(id_prod):
 @app.route('/editar_produto/<int:id_prod>', methods=['GET', 'POST'])
 def editar_produto(id_prod):
     if request.method == 'POST':
-        # Processa a atualização do produto
         nome_prod = request.form['nome_prod']
         fabricante = request.form['fabricante']
         dt_validade = request.form['dt_validade']
@@ -109,7 +103,6 @@ def editar_produto(id_prod):
         flash('Produto atualizado com sucesso!')
         return redirect(url_for('admin_access'))
     else:
-        # Exibe o formulário com os dados do produto para edição
         conn = get_db_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM TB_PRODUTOS WHERE ID_PROD = ?", id_prod)
@@ -118,7 +111,6 @@ def editar_produto(id_prod):
 
         return render_template('editar_produto.html', produto=produto)
 
-# Rotas para manipulação de clientes
 @app.route('/adicionar_cliente', methods=['POST'])
 def adicionar_cliente():
     nome = request.form['nome']
@@ -133,7 +125,6 @@ def adicionar_cliente():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # Verificar se o CPF já está cadastrado
     cursor.execute("SELECT CPF FROM TB_CLIENTES WHERE CPF = ?", cpf)
     if cursor.fetchone():
         flash('CPF já cadastrado!')
@@ -157,14 +148,12 @@ def excluir_cliente(id):
     flash('Cliente excluído com sucesso!')
     return redirect(url_for('admin_access'))
 
-# Rota de edição de cliente
 @app.route('/editar_cliente/<int:id>', methods=['GET', 'POST'])
 def editar_cliente(id):
     conn = get_db_connection()
     cursor = conn.cursor()
 
     if request.method == 'POST':
-        # Processa a atualização do cliente
         nome = request.form['nome']
         telefone = request.form['telefone']
         email = request.form['email']
@@ -185,7 +174,6 @@ def editar_cliente(id):
         return redirect(url_for('admin_access'))
 
     else:
-        # Exibe o formulário com os dados do cliente para edição
         cursor.execute("SELECT * FROM TB_CLIENTES WHERE ID = ?", id)
         cliente = cursor.fetchone()
         conn.close()
@@ -202,7 +190,6 @@ def produto(id_prod):
     conn.close()
 
     if produto:
-        # Retorna as informações do produto como JSON
         return jsonify({
             'ID_PROD': produto.ID_PROD,
             'NOME_PROD': produto.NOME_PROD,
